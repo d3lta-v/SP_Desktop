46bfc15c1b35e99d6c2e88b44d77ae5e6506b8e5
Pan Ziyue <random.rrr3@gmail.com>
2018-08-13 13:35:44 +0800

    fixed tslint warnings in post.ts

--- src/post.ts
+++ src/post.ts
@@ -4,23 +4,25 @@
  * and submitting it to the desired URL.
  * @param message The POST request's form-encoded fields as a Javascript Object
  */
-var onMessageHandler = function (message: any) {
+let onMessageHandler = function(message: any) {
   // Ensure it is run only once, as we will try to message twice
   chrome.runtime.onMessage.removeListener(onMessageHandler);
-  
+
   // code from https://stackoverflow.com/a/7404033/934239
-  var form = document.createElement("form");
+  const form = document.createElement("form");
   form.setAttribute("method", "post");
   form.setAttribute("action", message.url);
-  for (var key in message.data) {
-    var hiddenField = document.createElement("input");
-    hiddenField.setAttribute("type", "hidden");
-    hiddenField.setAttribute("name", key);
-    hiddenField.setAttribute("value", message.data[key]);
-    form.appendChild(hiddenField);
+  for (const key in message.data) {
+    if (message.data.hasOwnProperty(key)) {
+      const hiddenField = document.createElement("input");
+      hiddenField.setAttribute("type", "hidden");
+      hiddenField.setAttribute("name", key);
+      hiddenField.setAttribute("value", message.data[key]);
+      form.appendChild(hiddenField);
+    }
   }
   document.body.appendChild(form);
   form.submit();
-}
+};
 
 chrome.runtime.onMessage.addListener(onMessageHandler);
